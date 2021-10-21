5379153c0689588c8570d148ed273cc03e2f672c
alysson5g <alyssoncesartavares@gmail.com>
2021-10-21 01:54:04 -0400

    Adjust authentication middleware

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -5,17 +5,17 @@ import { promisify } from 'util';
 import authConfig from '../../config/auth';
 
 export default async (req, res, next) => {
-  console.log('middleware 1');
+
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    console.log('middleware 2');
+
 
     return res.status(401).json({ error: 'Token not provided' });
   }
 
   const [, token] = authHeader.split(' ');
-  console.log('middleware 3');
+
 
   try {
     const decoded = await promisify(Jwt.verify)(token, authConfig.secret);
@@ -23,7 +23,7 @@ export default async (req, res, next) => {
 
     req.userId = decoded.id;
 
-    console.log(req.userId);
+
 
     return next();
   } catch (err) {
