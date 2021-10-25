be657954b1911b900dad6c01831411d181ce66a6
alysson5g <alyssoncesartavares@gmail.com>
2021-10-25 16:24:22 -0400

    Config test

--- src/config/redisconfig.js
+++ src/config/redisconfig.js
@@ -1,14 +1,14 @@
 
-
+require('dotenv/config');
 const Redis = require('ioredis');
 
 const options = {
 
-  port: 7051,
+  port: process.env.REDISPORT,
   host: process.env.REDIS_HOST,
   password: process.env.REDIS_PASS,
 };
-
+console.log(process.env.REDIS_HOST);
 const redis = {
   client: new Redis({ ...options, db: 1 }),
 };
